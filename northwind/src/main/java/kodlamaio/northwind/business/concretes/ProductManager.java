a1e097553c2fc468ba72675dbd016e5e23d3bbd3
Katip <Katip@DESKTOP-6P2FP43>
2021-05-13 16:57:42 +0300

    northwind-project

---
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -0,0 +1,29 @@
+package kodlamaio.northwind.business.concretes;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
+import kodlamaio.northwind.entities.concretes.Product;
+
+@Service
+public class ProductManager implements ProductService{
+
+	private ProductDao _productDao;
+	
+	@Autowired
+	public ProductManager(ProductDao _productDao) {
+		super();
+		this._productDao = _productDao;
+	}
+	
+	@Override
+	public List<Product> getAll() {
+		
+		return this._productDao.findAll();
+	}
+
+}
