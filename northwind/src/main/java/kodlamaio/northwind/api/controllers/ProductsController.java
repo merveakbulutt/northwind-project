800ddcf4da6a96267b4eff79dde89de0bc4bb514
Katip <Katip@DESKTOP-6P2FP43>
2021-05-28 00:50:36 +0300

    JPA JPQL query added

--- northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
@@ -17,7 +18,8 @@ import kodlamaio.northwind.entities.concretes.Product;
 @RestController
 @RequestMapping("/api/products")
 public class ProductsController {
-
+	
+	
 	private ProductService productService;
 	
 	
@@ -26,7 +28,8 @@ public class ProductsController {
 		super();
 		this.productService = productService;
 	}
-	
+
+
 	@GetMapping("/getall")
 	public DataResult<List<Product>> getAll(){
 		return this.productService.getAll();
@@ -37,4 +40,35 @@ public class ProductsController {
 		return this.productService.add(product);
 	}
 	
+	@GetMapping("/getByProductName")
+	public DataResult<Product> getByProductName(@RequestParam String productName){
+		return this.productService.getByProductName(productName);
+	}
+	
+	@GetMapping("/getByProductNameAndCategoryId")
+	public DataResult<Product> 
+	getByProductNameAndCategoryId(@RequestParam("productName") String productName,@RequestParam("categoryId") int categoryId){
+		System.out.println(productName);
+		System.out.println(categoryId);
+		
+		return this.productService.getByProductNameAndCategoryId
+				(productName, categoryId);
+	}
+	
+	@GetMapping("/getByProductNameContains")
+	public DataResult<List<Product>> getByProductNameContains(@RequestParam String productName){
+		return this.productService.getByProductNameContains(productName);
+	}
+	
+	@GetMapping("/getAllByPage")
+	DataResult<List<Product>> getAll(int pageNo, int pageSize){
+		return this.productService.getAll(pageNo, pageSize);
+	}
+	
+	@GetMapping("/getAllDesc")
+	public DataResult<List<Product>> getAllSorted() {
+		return this.productService.getAllSorted();
+	}
+	  
+    
 }
